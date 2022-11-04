0708f3696f1735c49a886705508c4f4c21258d4d
mariano <mariano.poletti93@gmail.com>
2022-11-04 12:01:08 -0300

    entrega03

--- clase06/entrega03/index.js
+++ clase06/entrega03/index.js
@@ -1,8 +1,34 @@
+const Contenedor = require("./Contenedor");
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const contenedor = new Contenedor("productos.json");
 
+app.get('/', (req, res) => {
+    res.send('Hi Express Server!')
+});
 
-app.get('/productosRandom', (req,res) =>{
-    const num = Math.random()
-    const c1 = new Contenedor ('productos.txt')
-    const prod = c1getById(num)
-    res.send(prod)
+app.get('/productos', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    res.json(allProducts);
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    const maxId = allProducts.length;
+    const randomNumber = generateRandomNumber(1, maxId);
+    const randomProduct = await contenedor.getById(randomNumber);
+    res.json(randomProduct);
 })
+
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`Server iniciado http://localhost:${PORT}`)
+})
+
+server.on('error', (error) => console.log(error));
+
